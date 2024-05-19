a910bda05daf1da76235d83d000ddd4a7703e0c9
mattatzb <mateo.tatzber@epfl.ch>
2024-05-19 21:45:18 +0200

    Initial commit

---
+++ src/coordchempy/__init__.py
@@ -0,0 +1,5 @@
+"""Allows to visualize a coordination compound and to give some of its characteristics. The CSD code of the wanted complex is needed and it has to be in the data base used.."""
+
+from __future__ import annotations
+
+__version__ = "0.0.1"

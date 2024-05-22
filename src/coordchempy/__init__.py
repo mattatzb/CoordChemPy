36570e12402122e5db353e23ec80cc20888d4191
mattatzb <mateo.tatzber@epfl.ch>
2024-05-22 23:47:50 +0200

    Init file modification test

--- src/coordchempy/__init__.py
+++ src/coordchempy/__init__.py
@@ -3,3 +3,41 @@
 from __future__ import annotations
 
 __version__ = "0.0.1"
+
+from .coordchempy import (
+    calculate_distance,
+    get_covalent_radius,
+    read_lines_around_keyword,
+    read_xyz,
+    infer_bonds,
+    find_central_atom,
+    find_ligands,
+    find_actual_ligand_count,
+    calculate_angle,
+    determine_geometry,
+    calculate_angles_and_geometry,
+    cn,
+    charge,
+    visualize_label,
+    visualize,
+    visualize_all_data
+)
+
+__all__ = [
+    'calculate_distance',
+    'get_covalent_radius',
+    'read_lines_around_keyword',
+    'read_xyz',
+    'infer_bonds',
+    'find_central_atom',
+    'find_ligands',
+    'find_actual_ligand_count',
+    'calculate_angle',
+    'determine_geometry',
+    'calculate_angles_and_geometry',
+    'cn',
+    'charge',
+    'visualize_label',
+    'visualize',
+    'visualize_all_data'
+]
